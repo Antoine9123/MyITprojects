30a688e8e8609a72598d7e909b0070d5110769cf
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2023-11-30 11:25:07 +0100

    Navbar color change

--- navbar_script.js
+++ navbar_script.js
@@ -3,12 +3,19 @@ const WebPAGE = document.getElementById('WebPage');
 const AboutPAGE = document.getElementById('AboutPage');
 const ContactPAGE = document.getElementById('ContactPage');
 
+
 CodePAGE.addEventListener('click', () => {
     const scrollPosition = 0 * window.innerHeight / 100; // 50vh
     window.scrollTo({
         top: scrollPosition,
         behavior: 'smooth' 
     });
+    document.body.style.backgroundColor = '#540A0C'; 
+    document.body.style.color = '#E0A03F';
+    CodePAGE.style.color ='#E0A03F'
+    WebPAGE.style.color ='#E0A03F'
+    AboutPAGE.style.color ='#E0A03F'
+    ContactPAGE.style.color ='#E0A03F'
 });
 
 
@@ -19,6 +26,12 @@ WebPAGE.addEventListener('click', () => {
         top: scrollPosition,
         behavior: 'smooth' 
     });
+    document.body.style.backgroundColor = '#9E2A2B'; 
+    document.body.style.color = '#E0A03F';
+    CodePAGE.style.color ='#E0A03F'
+    WebPAGE.style.color ='#E0A03F'
+    AboutPAGE.style.color ='#E0A03F'
+    ContactPAGE.style.color ='#E0A03F'
 });
 
 
@@ -29,6 +42,12 @@ AboutPAGE.addEventListener('click', () => {
         top: scrollPosition,
         behavior: 'smooth'
     });
+    document.body.style.backgroundColor = '#E0A03F'; 
+    document.body.style.color = '#9E2A2B';
+    CodePAGE.style.color ='#9E2A2B'
+    WebPAGE.style.color ='#9E2A2B'
+    AboutPAGE.style.color ='#9E2A2B'
+    ContactPAGE.style.color ='#9E2A2B'
 });
 
 
@@ -39,4 +58,10 @@ ContactPAGE.addEventListener('click', () => {
         top: scrollPosition,
         behavior: 'smooth'
     });
+    document.body.style.backgroundColor = '#FFF3B0'; 
+    document.body.style.color = '#355C66';
+    CodePAGE.style.color ='#355C66'
+    WebPAGE.style.color ='#355C66'
+    AboutPAGE.style.color ='#355C66'
+    ContactPAGE.style.color ='#355C66'
 });
